e3027a9ae038bde9897a8149e0845e708ad83728
inhaq <hi@inhaq.me>
2021-02-15 09:26:48 +0500

    404 responsiveness

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,41 +1,69 @@
+/** @jsx jsx */
 import React from 'react';
-import { Box, Heading, Flex, Button, Grid } from 'theme-ui';
-
+import { Box, Heading, Flex, Button, jsx } from 'theme-ui';
+import { Link } from 'gatsby';
 import SEO from '../components/seo';
-import ErrorIlustration from './../images/ErrorIlustration';
+
+import ErrorIllustration from './../images/ErrorIlustration';
 import Ellipse from './../images/Ellipse';
 import EFiveEllipses from './../images/EFiveEllipses';
 import EllipsesRotated from './../images/EllipsesRotated';
-import EllipseHalf from './../images/EllipseHalf';
 
 const NotFoundPage = () => (
-  <React.Fragment>
+  <Box
+    sx={{
+      background: `linear-gradient(
+      0deg
+    , rgb(255, 255, 255) 0%, rgb(228, 229, 255) 25%, rgb(244, 245, 255) 100%)`,
+      py: ['60px'],
+    }}
+  >
     <SEO title="404: Not found" />
     <Flex
       sx={{
-        position: 'relative',
-        marginTop: 60,
+        margin: '0 auto',
+        maxWidth: '1110px',
       }}
     >
-      <span sx={{ position: 'absolute' }}>
+      <span
+        sx={{
+          position: ['absolute'],
+          display: ['none', 'block'],
+        }}
+      >
         <Ellipse />
       </span>
+
+      <div
+        sx={{
+          mt: ['-80px', '-20px'],
+          mx: 'auto',
+        }}
+      >
+        <ErrorIllustration
+          sx={{ maxWidth: ['300px', '400px', '500px', '600px'], ml: '-30px' }}
+        />
+      </div>
 
       <span
         sx={{
           position: 'absolute',
+          right: '30px',
+          display: ['none', 'none', 'block'],
         }}
       >
-        <ErrorIlustration />
-      </span>
-
-      <span sx={{ position: 'absolute', left: 1226, top: 170 }}>
         <EFiveEllipses />
       </span>
     </Flex>
 
     <Flex>
-      <span sx={{ position: 'absolute' }}>
+      <span
+        sx={{
+          position: 'absolute',
+          left: 0,
+          display: ['none', 'block'],
+        }}
+      >
         <EllipsesRotated />
       </span>
       <Flex
@@ -43,16 +71,15 @@ const NotFoundPage = () => (
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
-          flex: 1,
+          margin: 'auto',
+          mt: [0, 0, '20px'],
         }}
       >
         <Heading
           as={'h1'}
           sx={{
-            width: 173,
-            height: 70,
             fontWeight: 'bold',
-            fontSize: 64,
+            fontSize: [28, 34, 44, 64],
           }}
         >
           Oops!
@@ -61,24 +88,21 @@ const NotFoundPage = () => (
         <Heading
           as={'h2'}
           sx={{
-            fontSize: 36,
+            fontSize: [18, 24, 24, 24],
             lineHeight: 3,
             fontWeight: 600,
           }}
         >
           We couldn't find that page.
         </Heading>
-
-        <Button sx={{ variant: 'buttons.prime', width: ['100%', 212] }}>
-          Back to Home
-        </Button>
+        <Link to={'/'} style={{ textDecoration: 'none' }}>
+          <Button sx={{ variant: 'buttons.prime', width: ['100%', 212] }}>
+            Back to Home
+          </Button>
+        </Link>
       </Flex>
-
-      <span sx={{ position: 'absolute' }}>
-        <EllipseHalf />
-      </span>
     </Flex>
-  </React.Fragment>
+  </Box>
 );
 
 export default NotFoundPage;
