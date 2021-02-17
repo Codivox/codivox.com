1bc6c967398913c7d293668ff06b2399d2451c53
inhaq <hi@inhaq.me>
2021-02-17 23:32:52 +0500

    design page update

--- src/components/Design/appDesign.tsx
+++ src/components/Design/appDesign.tsx
@@ -1,17 +1,17 @@
 /** @jsx jsx */
 import { jsx, Box, Flex, Heading } from 'theme-ui';
 import React from 'react';
-import AppDesignImg from '../../images/appDesignImg';
 import Oval1x from '../../images/oval1x';
 import Oval2x from '../../images/oval2x';
+import AppDesignImg from '../../images/appDesignImg';
 
 const AppDesign: React.FC = () => {
   const brandTitle =
-    'App design that’s not just stunning, but a truly engaging product';
+    'Apps design, that’s not just stunning but a truly engaging product';
   const brandSubtitle =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In imperdiet dui non ex maximus varius. Fusce interdum felis at aliquet maximus. Vivamus tempor mauris ut molestie cursusm a.';
+    'We design stunning and exclusive layouts for Android and iOS apps that enhances user experience.';
   return (
-    <div sx={{ background: 'rgba(244, 245, 255, 0.5)', minHeight: '685px' }}>
+    <div sx={{ background: 'rgba(244, 245, 255, 0.5)', minHeight: '500px' }}>
       <Box
         sx={{
           m: '0 auto',
@@ -22,30 +22,27 @@ const AppDesign: React.FC = () => {
         <hr
           sx={{
             border: '1px solid #A7ADFF',
+            opacity: 0.2,
           }}
         />
         <Box sx={{ textAlign: 'center', pt: '77px' }}>
           <Heading
             sx={{
-              fontSize: ['26px', '38px'],
-              width: ['100%', 684],
+              fontSize: ['26px', '32px'],
+              width: ['100%', 440],
               m: '0 auto',
               fontWeight: 600,
             }}
           >
-            App design that provides unique experience across platforms
+            Apps design that provides unique experience across platforms ⚡️
           </Heading>
-          <p sx={{ opacity: '0.85', width: ['100%', 430], m: '0 auto' }}>
-            We design stunning and exclusive layouts for Android and iOS apps
-            that enhances user experience.
-          </p>
 
           <Flex sx={{ position: 'absolute', display: ['none', 'block'] }}>
             <Box sx={{ position: 'relative', top: '-150px' }}>
-              <span sx={{ position: 'relative', left: '752px', top: '6px' }}>
+              <span sx={{ position: 'relative', left: '300px', top: '49px' }}>
                 <Oval1x />
               </span>
-              <span sx={{ position: 'relative', left: '148px', top: '-27px' }}>
+              <span sx={{ position: 'relative', left: '495px' }}>
                 <Oval2x />
               </span>
             </Box>
@@ -66,7 +63,7 @@ const AppDesign: React.FC = () => {
             sx={{
               display: 'grid',
               gridTemplateRows: 'auto auto auto',
-              height: ['calc( 300px )', 'calc( 400px )'],
+              // height: ['calc( 300px )', 'calc( 400px )'],
               gridTemplateColumns: ['100%'],
               gridColumn: [1, null],
               textAlign: ['center', 'left'],
@@ -74,30 +71,37 @@ const AppDesign: React.FC = () => {
           >
             <Heading
               sx={{
-                fontSize: ['27px', '28px'],
-                maxWidth: '520px',
-                mt: ['10px', '100px'],
+                fontSize: ['24px', '26px'],
+                display: 'flex',
+                justifyContent: 'center',
               }}
             >
               {brandTitle}
             </Heading>
-            <p sx={{ opacity: '0.85', mt: ['10px', '-35px'] }}>
-              {brandSubtitle}
-            </p>
+            <p sx={{ opacity: '0.85' }}>{brandSubtitle}</p>
           </Flex>
-          <AppDesignImg
+          <Flex
             sx={{
-              gridRow: [1, 'unset'],
-              gridColumn: [1, 'unset'],
-              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
-          />
+          >
+            <AppDesignImg
+              sx={{
+                gridRow: [1, 'unset'],
+                gridColumn: [1, 'unset'],
+                maxWidth: '250px',
+                maxHeight: '200px',
+              }}
+            />
+          </Flex>
         </Box>
         <hr
           sx={{
             border: '1px solid #A7ADFF',
             position: 'relative',
-            bottom: '-33px',
+            bottom: '-60px',
+            opacity: '0.2',
           }}
         />
       </Box>
